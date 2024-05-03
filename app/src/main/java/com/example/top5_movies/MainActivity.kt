0ed48befd049c8ff4f3eefb2a013e2569a5514db
Gordon McCrone <gordonmccrone@Gordons-MacBook-Pro.local>
2024-05-03 19:48:10 +1200

    Uodate and Delete buttons work

--- app/src/main/java/com/example/top5_movies/MainActivity.kt
+++ app/src/main/java/com/example/top5_movies/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.top5_movies
 
-import android.app.Activity
-import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
@@ -9,7 +7,6 @@ import android.util.DisplayMetrics
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.AbsListView
 import android.widget.AdapterView
 import android.widget.Button
@@ -17,18 +14,14 @@ import android.widget.ListView
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import com.example.top5_movies.databinding.ActivityMainBinding
-import com.example.top5_movies.databinding.ListviewItemBinding
 import com.example.top5_movies.databinding.NewStringInputBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var activityMainBinding: ActivityMainBinding
     lateinit var background: RelativeLayout
 
     var movieItems: MutableList<MovieItems> = ArrayList()
-    //var movieItemsOriginal: MutableList<MovieItems> = ArrayList()
 
     val iOSBlue = "#2D7CF6"
     val iOSGrey = "#F9F9F9"
@@ -46,6 +39,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var adapter: MovieItemsListAdapter
     lateinit var movieItemsListView: ListView
     lateinit var buttonCreate: Button
+    var listTapOperation = ButtonOperation.OPEN
+    lateinit var buttonUpdate: Button
+    lateinit var buttonDelete: Button
 
     private lateinit var bindingNewStringInputBinding: NewStringInputBinding
 
@@ -55,8 +51,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val listTapOperation = ButtonOperation.OPEN
 
         val buttonWidth = 300
         val bottomMargin = 80
@@ -95,8 +89,58 @@ class MainActivity : AppCompatActivity() {
         movieItemsListView.setAdapter(adapter)
 
         movieItemsListView.setOnItemClickListener(AdapterView.OnItemClickListener { parent, view, position, id ->
-            val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            //val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            //inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+
+            if (listTapOperation == ButtonOperation.OPEN) {
+                // TODO Open up Search movie page
+            }
+            else if (listTapOperation == ButtonOperation.UPDATE) {
+                val gdButtonUpdate = GradientDrawable()
+                gdButtonUpdate.setColor(Color.parseColor(iOSYellow)) // Changes this drawable to use a single color instead of a gradient
+                gdButtonUpdate.cornerRadius = 20f
+                gdButtonUpdate.setStroke(5, Color.parseColor("#000000"))
+                buttonUpdate.background = gdButtonUpdate
+
+                listTapOperation = ButtonOperation.OPEN
+
+                bindingNewStringInputBinding = NewStringInputBinding.inflate(LayoutInflater.from(this))
+                val view = bindingNewStringInputBinding.root
+                setContentView(view)
+
+                //val currentMovieItem = parent.selectedItem
+                val currentMovieItem = parent.getItemAtPosition(position)
+                currentMovieItem as MovieItems
+                bindingNewStringInputBinding.label.setText(currentMovieItem.name)
+
+                bindingNewStringInputBinding.buttonSave.setOnClickListener {
+                    if (bindingNewStringInputBinding.label.text.toString().length > 0) {
+                        movieItems.set(position, MovieItems(bindingNewStringInputBinding.label.text.toString()))
+                        //movieItems.set(position, MovieItems("test"))
+                        setContentView(R.layout.activity_main)
+                        adapter.notifyDataSetChanged()
+                        setContentView(background)
+                    }
+                }
+
+                bindingNewStringInputBinding.buttonCancel.setOnClickListener{
+                    setContentView(R.layout.activity_main)
+                    setContentView(background)
+                }
+            }
+            else if (listTapOperation == ButtonOperation.DELETE) {
+                val gdButtonDelete = GradientDrawable()
+                gdButtonDelete.setColor(Color.parseColor(iOSYellow)) // Changes this drawable to use a single color instead of a gradient
+                gdButtonDelete.cornerRadius = 20f
+                gdButtonDelete.setStroke(5, Color.parseColor("#000000"))
+                buttonDelete.background = gdButtonDelete
+
+                listTapOperation = ButtonOperation.OPEN
+
+                movieItems.removeAt(position)
+
+                adapter.notifyDataSetChanged()
+            }
         })
 
         movieItemsListView.setOnScrollListener(object : AbsListView.OnScrollListener {
@@ -121,7 +165,6 @@ class MainActivity : AppCompatActivity() {
         buttonCreate.setLayoutParams(rel_buttonCreate)
         buttonCreate.text = "Create"
         buttonCreate.setOnClickListener{
-            Log.d("*5*", "buttonCreate.setOnClickListener")
             onClickButtonCreate()
         }
         buttonCreate.setTextColor(Color.parseColor("#000000"))
@@ -132,6 +175,48 @@ class MainActivity : AppCompatActivity() {
         gdButtonCreate.setStroke(5, Color.parseColor("#000000"))
         buttonCreate.background = gdButtonCreate
         background.addView(buttonCreate)
+
+        buttonUpdate = Button(this)
+//        button.setPadding(0,0,100,40)
+        val rel_buttonUpdate = RelativeLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+        rel_buttonUpdate.leftMargin = leftMargin + buttonWidth + buttonSpacer
+        rel_buttonUpdate.topMargin = displayMetrics.heightPixels - buttonHeight - bottomMargin
+        rel_buttonUpdate.width = buttonWidth
+        rel_buttonUpdate.height = buttonHeight
+        buttonUpdate.setLayoutParams(rel_buttonUpdate)
+        buttonUpdate.text = "Update"
+        buttonUpdate.setOnClickListener{
+            onClickButtonUpdate()
+        }
+        buttonUpdate.setTextColor(Color.parseColor("#000000"))
+        buttonUpdate.visibility = View.VISIBLE
+        val gdButtonUpdate = GradientDrawable()
+        gdButtonUpdate.setColor(Color.parseColor(iOSYellow)) // Changes this drawable to use a single color instead of a gradient
+        gdButtonUpdate.cornerRadius = 20f
+        gdButtonUpdate.setStroke(5, Color.parseColor("#000000"))
+        buttonUpdate.background = gdButtonUpdate
+        background.addView(buttonUpdate)
+
+        buttonDelete = Button(this)
+//        button.setPadding(0,0,100,40)
+        val rel_buttonDelete = RelativeLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+        rel_buttonDelete.leftMargin = leftMargin + buttonWidth + buttonSpacer + buttonWidth + buttonSpacer
+        rel_buttonDelete.topMargin = displayMetrics.heightPixels - buttonHeight - bottomMargin
+        rel_buttonDelete.width = buttonWidth
+        rel_buttonDelete.height = buttonHeight
+        buttonDelete.setLayoutParams(rel_buttonDelete)
+        buttonDelete.text = "Delete"
+        buttonDelete.setOnClickListener{
+            onClickButtonDelete()
+        }
+        buttonDelete.setTextColor(Color.parseColor("#000000"))
+        buttonDelete.visibility = View.VISIBLE
+        val gdButtonDelete = GradientDrawable()
+        gdButtonDelete.setColor(Color.parseColor(iOSYellow)) // Changes this drawable to use a single color instead of a gradient
+        gdButtonDelete.cornerRadius = 20f
+        gdButtonDelete.setStroke(5, Color.parseColor("#000000"))
+        buttonDelete.background = gdButtonDelete
+        background.addView(buttonDelete)
     }
     fun setupMovieItems() {
         movieItems.add(MovieItems("Action"))
@@ -158,6 +243,51 @@ class MainActivity : AppCompatActivity() {
         bindingNewStringInputBinding.buttonCancel.setOnClickListener{
             setContentView(R.layout.activity_main)
             setContentView(background)
+        }
+    }
+
+    fun onClickButtonUpdate() {
+        if (listTapOperation == ButtonOperation.OPEN) {
+            val gdButtonUpdate = GradientDrawable()
+            gdButtonUpdate.setColor(Color.parseColor(iOSRed)) // Changes this drawable to use a single color instead of a gradient
+            gdButtonUpdate.cornerRadius = 20f
+            gdButtonUpdate.setStroke(5, Color.parseColor("#000000"))
+            buttonUpdate.background = gdButtonUpdate
+
+            listTapOperation = ButtonOperation.UPDATE
+        }
+        else {
+            val gdButtonUpdate = GradientDrawable()
+            gdButtonUpdate.setColor(Color.parseColor(iOSYellow)) // Changes this drawable to use a single color instead of a gradient
+            gdButtonUpdate.cornerRadius = 20f
+            gdButtonUpdate.setStroke(5, Color.parseColor("#000000"))
+            buttonUpdate.background = gdButtonUpdate
+
+            listTapOperation = ButtonOperation.OPEN
+        }
+    }
+
+    fun onClickButtonDelete() {
+        Log.d("*5*", "onClickButtonDelete")
+
+        if (listTapOperation == ButtonOperation.OPEN) {
+
+            val gdButtonDelete = GradientDrawable()
+            gdButtonDelete.setColor(Color.parseColor(iOSRed)) // Changes this drawable to use a single color instead of a gradient
+            gdButtonDelete.cornerRadius = 20f
+            gdButtonDelete.setStroke(5, Color.parseColor("#000000"))
+            buttonDelete.background = gdButtonDelete
+
+            listTapOperation = ButtonOperation.DELETE
+        }
+        else {
+            val gdButtonDelete = GradientDrawable()
+            gdButtonDelete.setColor(Color.parseColor(iOSYellow)) // Changes this drawable to use a single color instead of a gradient
+            gdButtonDelete.cornerRadius = 20f
+            gdButtonDelete.setStroke(5, Color.parseColor("#000000"))
+            buttonDelete.background = gdButtonDelete
+
+            listTapOperation = ButtonOperation.OPEN
         }
     }
 }
